708e9eb57aeb4c245a9bf5e42a31431ab414c021
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-09-12 12:00:20 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 SNAPSHOT_FOLDER=./data/snapshots
-URL=https://github.com/andy-backup/fibos/raw/master/snapshot.bin
+URL=$(curl api.fibos123.com/last_snapshot)
 wget $URL -P $SNAPSHOT_FOLDER
 export SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER/*.bin -t | head -1)
 rm -rf ./data/blocks ./data/state
