feb94c2af1444cbaa37207a3cc03af95dc52feae
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-09-12 17:22:16 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 SNAPSHOT_FOLDER=./data/snapshots
+rm -rf ./data/blocks ./data/state ./data/snapshots/*
 URL=$(curl api.fibos123.com/last_snapshot)
 wget $URL -P $SNAPSHOT_FOLDER
 export SNAPSHOT_FILE=$(ls $SNAPSHOT_FOLDER/*.bin -t | head -1)
-rm -rf ./data/blocks ./data/state
-find $SNAPSHOT_FOLDER/* -mtime +30
-find $SNAPSHOT_FOLDER/* -mtime +30 -delete
 
 fibos /fibos/start.js
