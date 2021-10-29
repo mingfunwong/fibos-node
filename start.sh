4888a8edc250655fd18203067e0fec54adf80dbf
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-10-29 22:47:18 +0900

    fix start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,15 @@ SNAPSHOT_FOLDER=./data/snapshots
 rm -rf ./data/*
 
 if [ "$SNAPSHOT_ENABLE" = "true" ]; then
-  mkdir ./data ./data/snapshots
+
+  if [ ! -d "./data" ];then
+    mkdir ./data
+  fi
+
+  if [ ! -d "./data/snapshots" ];then
+    mkdir ./data/snapshots
+  fi
+
   if [ ! $SNAPSHOT_URL ]; then  
     SNAPSHOT_URL=$(curl api.fibos123.com/last_snapshot)
   fi
